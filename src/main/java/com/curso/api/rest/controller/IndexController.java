2b6f0816756286afb079a593275afb93ca0e6740
Rebeca <becatriz7@gmail.com>
2020-05-05 22:15:52 -0400

    Metodo Post e Delete

--- src/main/java/com/curso/api/rest/controller/IndexController.java
+++ src/main/java/com/curso/api/rest/controller/IndexController.java
@@ -7,11 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.curso.api.rest.model.Usuario;
 import com.curso.api.rest.repository.UsuarioRepository;
@@ -40,7 +36,15 @@ public class IndexController {
 		
 		return new ResponseEntity<Usuario>(usuario.get(),  HttpStatus.OK);
 	}
-	
+
+
+	@GetMapping(value = "/{id}/codigovenda/{idvenda}", produces = "application/json")
+	public ResponseEntity<Usuario> relatorio(@PathVariable(value = "id") Long id, @PathVariable (value = "idvenda") Long idvenda) {
+
+		Optional<Usuario> usuario = usuarioRepository.findById(id);
+
+		return new ResponseEntity<Usuario>(usuario.get(),  HttpStatus.OK);
+	}
 	
 	
 	@GetMapping(value = "/", produces = "application/json")
@@ -50,9 +54,35 @@ public class IndexController {
 		
 		return new ResponseEntity<List<Usuario>>(list,  HttpStatus.OK);
 	}
-	
-	
-	
-	
+
+	@PostMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario>cadastrar(@RequestBody Usuario usuario){
+
+
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+
+	}
+
+	@PostMapping(value = "/{iduser}/idvenda/{idvenda}", produces = "application/json")
+	public ResponseEntity<Usuario>cadastrarvenda(@PathVariable Long iduser, @PathVariable Long idvenda){
+
+		//Usuario usuarioSalvo = usuarioRepository.save(usuario);
+
+
+		return new ResponseEntity(" id user:" + iduser + " id venda:  " +idvenda, HttpStatus.OK);
+
+	}
+
+	@DeleteMapping(value = "/{iduser}", produces = "application/json")
+	public ResponseEntity<Usuario>deleteusuario(@PathVariable Long iduser){
+
+		 usuarioRepository.deleteById(iduser);
+
+		return new ResponseEntity("Usuario deletado id: " + iduser, HttpStatus.OK);
+
+	}
+
 
 }
